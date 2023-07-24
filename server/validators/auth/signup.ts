359aa44afd693f9162a565c0680a029595838b45
mahdiHash <mahdihashemi.code@gmail.com>
2023-07-24 14:56:50 +0330

    refactor(validator): err messages, password confirm on signup

--- server/validators/auth/signup.ts
+++ server/validators/auth/signup.ts
@@ -1,23 +1,19 @@
-import { object, string } from 'yup';
+import { ref, object, string } from 'yup';
 
 const schema = object({
   email: string()
-    .required('ایمیل باید وارد شود.')
+    .required('ایمیل باید وارد شود')
     .trim()
-    .email('ایمیل معتبر نیست.'),
+    .email('ایمیل معتبر نیست'),
   
   password: string()
-    .required('رمز عبور باید وارد شود.')
+    .required('رمز عبور باید وارد شود')
     .trim()
-    .min(8, 'رمز عبور باید حداقل ۸ کاراکتر داشته باشد.'),
+    .min(8, 'رمز عبور باید حداقل ۸ کاراکتر داشته باشد'),
 
-  passwordConfirm: string().required().test({
-    name: 'mathPass',
-    message: 'رمز عبور وارد شده یکسان نیست.',
-    test: function () {
-      this.parent.password === this.parent.passwordConfirm;
-    }
-  }),
+  passwordConfirm: string()
+    .required('تکرار رمز عبور باید وارد شود')
+    .oneOf([ref('password')], 'رمز عبور یکسان نیست'),
 });
 
 export { schema as signupInpValidator };
