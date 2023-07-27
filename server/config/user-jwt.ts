8285dc75cacb2d48b769760223832ce3661fde09
mahdiHash <mahdihashemi.code@gmail.com>
2023-07-27 19:19:40 +0330

    chore(config): add config

--- server/config/user-jwt.ts
+++ server/config/user-jwt.ts
@@ -3,7 +3,7 @@ import { jwtExtractorFromCookie, decrypt } from '../helpers';
 import { envVars } from './envVars';
 import { JwtPayload } from 'jsonwebtoken';
 import { UserService } from '../services';
-import { UnauthorizedErr } from '../helpers/errors';
+import { NotFoundErr, UnauthorizedErr } from '../helpers/errors';
 
 const jwtStrategy = new Strategy(
   {
@@ -12,13 +12,21 @@ const jwtStrategy = new Strategy(
   },
   async (payload: JwtPayload, cb: VerifiedCallback) => {
     const email = decrypt(payload.email) as string;
-    const user = await UserService.getUserByEmail(email);
 
-    if (user === null) {
-      cb(new UnauthorizedErr('هویت شما مشخص نیست'));
-    }
-    else {
-      cb(null, user);
+    try {
+      const user = await UserService.getUserByEmail(email);
+
+      if (user === null) {
+        cb(new UnauthorizedErr('هویت شما مشخص نیست'));
+      } else {
+        cb(null, user);
+      }
+    } catch (err) {
+      if (err instanceof NotFoundErr) {
+        cb(new UnauthorizedErr('هویت شما مشخص نیست'));
+      } else {
+        throw err;
+      }
     }
   }
 );
